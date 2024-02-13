1d649d7a2ce74553edff48a7a6b691c7afafc120
0MEGA <0mega.prog987@gmail.com>
2024-02-13 15:03:58 +0100

    comment update

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 
 #please note that this damn god forbiden load .env function can only work with properly set up library´s
 #if you try to do it otherwise and not like it says in README then it´s your fault and you can fuck with that on your own
+#note aswell that i am not planning to fix it
 load_dotenv()
 token = os.getenv('TOKEN')
 
