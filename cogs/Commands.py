0c8d6cb6941e9633925b54e2acef26d7f03da1d8
0MEGA <0mega.prog987@gmail.com>
2024-03-02 01:12:39 +0100

    help message updated

--- cogs/Commands.py
+++ cogs/Commands.py
@@ -73,25 +73,30 @@ Commands format:
 
 Available commands:
     Functionality:
-        ``test`` [t] - Test if the bot is active (a basic ping pong command)
+        ``Ping`` [test] - Test if the bot is active (a basic ping pong command)
         ``help`` - Shows this message
         ``reload`` - reloads any cog
         ``load``- loads a new cog module on the go
         ``setprefix`` - lets the user define prefix
         ``debug`` [db] - toggles debug mode on and off
-        ``reloadconfig`` [reloadcfg] - reloads the main config module(not working as of right now)
+        ``restart`` - Restarts the entire bot
+        ``logout`` - logs the bot out
+        ``github`` - sends my github link
 
     Activity:
         ``ON`` [on] - Sets RPC to your default (can be customized in code)
         ``invisible`` [off] - Sets status to invisible (nothing special)
         ``work``[worki] - worki work RPC, just for fun (Do Not Disturb status)
         ``suffer``[evd] - listening to voices in my head RPC (idle status)
+        ``rpc`` - accepts arguments such as watching, listening etc. and a custom status message to that
+        ``status`` - sets your status without changing the rpc
 
     Emotes:
         ``emote`` [e] - Automated emote integration without having Discord Nitro (works on mobile aswell)
         ``addemote``[addE] - adds a new emote to emotes.json file, you will need emote name its id and format 
         ``removeemote``[rmE] - removes emote entry from emotes.json file
         ``Find``[findEmote] - allowes you to find emote id/format and create your own shortcut
+        `èmotelist`` - lists all the emote names saven in the emotes.json file
 
     List of emotes:
         <insert your emote shortcuts here>
